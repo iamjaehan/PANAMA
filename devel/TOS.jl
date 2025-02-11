c103734c9728df4aae1839fae7577e68c6aa6db8
jaehan im <jaehan.im@utexas.edu>
2025-02-11 00:15:50 -0600

    add TOS init

---
+++ devel/TOS.jl
@@ -0,0 +1,67 @@
+module TOS
+
+using Dates
+
+# Define the Flight structure
+struct Flight
+    flight_id::String
+    origin::String
+    destination::String
+    earliest_departure::DateTime
+    trajectory_options::Vector{Dict{String, Any}}
+end
+
+# Constructor for Flight
+function Flight(flight_id::String, origin::String, destination::String, earliest_departure::DateTime)
+    return Flight(flight_id, origin, destination, earliest_departure, Vector{Dict{String, Any}}())
+end
+
+# Add trajectory option to a flight
+function add_trajectory_option!(flight::Flight, route::String, altitude::Int, speed::Int, rtc::Int, valid_start::DateTime, valid_end::Union{DateTime, Nothing}, required_notice::Union{Int, Nothing})
+    option = Dict(
+        "route" => route,
+        "altitude" => altitude,
+        "speed" => speed,
+        "relative_trajectory_cost" => rtc,
+        "valid_start_time" => valid_start,
+        "valid_end_time" => valid_end,
+        "required_notice_time" => required_notice
+    )
+    push!(flight.trajectory_options, option)
+end
+
+# Simulate data input
+function generate_flight_data()
+    flight = Flight("FL123", "ORD", "LGA", DateTime(2023, 4, 25, 13, 0))
+
+    # Example trajectory options
+    add_trajectory_option!(flight, "ORD..ELX..JHW..RKA..LGA", 350, 480, 0, DateTime(2023, 4, 25, 13, 0), nothing, nothing)
+    add_trajectory_option!(flight, "ORD..ELX..JHW..RKA..LGA", 370, 480, 10, DateTime(2023, 4, 25, 13, 0), DateTime(2023, 4, 25, 15, 0), nothing)
+    add_trajectory_option!(flight, "ORD..TVC..RKA..IGN..LGA", 350, 480, 20, DateTime(2023, 4, 25, 13, 0), DateTime(2023, 4, 25, 16, 0), 45)
+
+    return flight
+end
+
+# Evaluate costs for trajectory options
+function evaluate_trajectory_costs!(flight::Flight)
+    for option in flight.trajectory_options
+        option["total_cost"] = option["relative_trajectory_cost"]  # Extend with other factors if needed
+    end
+end
+
+# Generate and print TOS
+function generate_tos(flight::Flight)
+    println("Submitting TOS for flight: ", flight.flight_id)
+    for option in flight.trajectory_options
+        println(option)
+    end
+end
+
+# Main workflow to run TOS generation
+function tos_workflow()
+    flight_data = generate_flight_data()
+    evaluate_trajectory_costs!(flight_data)
+    generate_tos(flight_data)
+end
+
+end # module TOS_devel
--- src/PANAMA.jl
+++ src/PANAMA.jl
@@ -2,4 +2,7 @@ module PANAMA
 
 greet() = print("Hello World!")
 
+include("..//devel/TOS.jl")
+using .TOS
+
 end # module PANAMA
