696db3e07ce4bced073d24824f9e08073c368275
jaehan im <jaehan.im@utexas.edu>
2025-02-12 12:05:37 -0600

    add CTB

--- devel/CTB.jl
+++ devel/CTB.jl
@@ -2,7 +2,29 @@
 
 module CTB
 using Dates
+import ..TOS: Flight, add_trajectory_option!
 
-
-
+function generateCtb(tos_list::Vector{Flight}, weights::Vector{Float64})
+    # Solve MDO problem with weights
+    # Return a CTB
 end # module CTB
+
+
+function generateCtbSet(tos_list::Vector{Flight})
+    iterNum = 10
+    ctbSet = []
+
+    # Generate weights
+    weights = rand(iterNum)
+    weights = weights / sum(weights)
+
+    # Generate CTBs
+    for i = 1:iterNum
+        # Generate a CTB
+        ctb = generateCtb(tos_list, weights)
+        push!(ctbSet, ctb)
+    end
+
+    # Return the set of CTBs
+    return ctbSet
+end
